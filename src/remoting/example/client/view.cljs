bf625747c927752ebfa6b9cd051d9fe216f4815e
223kazuki <rixi223.kazuki@gmail.com>
2016-12-17 16:48:15 +0900

    Use sablono in the view.

--- src/remoting/example/client/view.cljs
+++ src/remoting/example/client/view.cljs
@@ -1,19 +1,21 @@
 (ns remoting.example.client.view
   (:require [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [sablono.core :refer-macros [html]]))
 
 (defn result-list [results]
-  (dom/ul #js {:key "result-list"}
-          (map #(dom/li #js {:key %} nil %) results)))
+  (html
+    [:ul {:key "result-list"}
+     (map #(vec [:li {:key %} %]) results)]))
 
 (defn search-field [ac query]
-  (dom/input
-    #js {:key "search-field"
-         :value query
-         :onChange
-         (fn [e]
-           (om/set-query! ac
-                          {:params {:query (.. e -target -value)}}))}))
+  (html
+    [:input
+     {:key "search-field"
+      :value query
+      :onChange
+      (fn [e]
+        (om/set-query! ac
+                       {:params {:query (.. e -target -value)}}))}]))
 
 (defui AutoCompleter
   static om/IQueryParams
@@ -25,8 +27,9 @@
   Object
   (render [this]
           (let [{:keys [search/results]} (om/props this)]
-            (dom/div nil
-                     (dom/h2 nil "Autocompleter?")
-                     (cond->
-                       [(search-field this (:query (om/get-params this)))]
-                       (not (empty? results)) (conj (result-list results)))))))
+            (html
+              [:div 
+               [:h2 "Autocompleter?"]
+               (cond->
+                 [(search-field this (:query (om/get-params this)))]
+                 (not (empty? results)) (conj (result-list results)))]))))
